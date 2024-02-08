a66494127c78e97967b4e13b068396e8c370f27b
Felix Dommes <f.dommes@famedly.com>
2024-02-08 12:26:17 +0100

    chore: Add traditional BP256R1 PEM

--- src/util/oid.rs
+++ src/util/oid.rs
@@ -48,8 +48,8 @@ pub static OID_X25519: LazyLock<ObjectIdentifier> =
 pub static OID_X448: LazyLock<ObjectIdentifier> =
     LazyLock::new(|| ObjectIdentifier::from_slice(&[1, 3, 101, 111]));
 
-pub static OID_BP256R1: Lazy<ObjectIdentifier> =
-    Lazy::new(|| ObjectIdentifier::from_slice(&[1, 3, 36, 3, 3, 2, 8, 1, 1, 7]));
+pub static OID_BP256R1: LazyLock<ObjectIdentifier> =
+    LazyLock::new(|| ObjectIdentifier::from_slice(&[1, 3, 36, 3, 3, 2, 8, 1, 1, 7]));
 
 #[derive(Debug, Eq, PartialEq)]
 pub struct ObjectIdentifier {
